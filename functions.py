15a3c9df69f15d34570704657dbcdef82a919a3e
interstellarninja <ninjainterstellar@gmail.com>
2024-03-12 17:34:36 +0000

    adding google search and scrape function and updating code interpreter

--- functions.py
+++ functions.py
@@ -1,8 +1,98 @@
+import re
+import inspect
+import requests
 import pandas as pd
 import yfinance as yf
+import concurrent.futures
+
 from typing import List
+from bs4 import BeautifulSoup
+from utils import inference_logger
 from langchain.tools import tool
 from langchain_core.utils.function_calling import convert_to_openai_tool
+
+@tool
+def code_interpreter(code_markdown: str) -> dict:
+    """
+    Execute the provided Python code string on the terminal using exec.
+    The string should contain valid, executable and pure python code in markdown syntax.
+    Code should also import any required python packages.
+
+    Parameters:
+    - code_markdown (str): The Python code with markdown syntax to be executed.
+      for eg. ```python\n<code-string>\n```
+
+    Returns:
+    dict: A dictionary containing variables declared and values returned by function calls.
+
+    Note: Use this function with caution, as executing arbitrary code can pose security risks.
+    """
+    try:
+        # Extracting code from Markdown code block
+        code_lines = code_markdown.split('\n')[1:-1]
+        code_without_markdown = '\n'.join(code_lines)
+
+        # Create a new namespace for code execution
+        exec_namespace = {}
+
+        # Execute the code in the new namespace
+        exec(code_without_markdown, exec_namespace)
+
+        # Collect variables and function call results
+        result_dict = {}
+        for name, value in exec_namespace.items():
+            if callable(value):
+                try:
+                    result_dict[name] = value()
+                except TypeError:
+                    # If the function requires arguments, attempt to call it with arguments from the namespace
+                    arg_names = inspect.getfullargspec(value).args
+                    args = {arg_name: exec_namespace.get(arg_name) for arg_name in arg_names}
+                    result_dict[name] = value(**args)
+            elif not name.startswith('_'):  # Exclude variables starting with '_'
+                result_dict[name] = value
+
+        return result_dict
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+@tool
+def google_search_and_scrape(query: str) -> dict:
+    """
+    Performs a Google search for the given query, retrieves the top search result URLs,
+    and scrapes the text content and table data from those pages in parallel.
+
+    Args:
+        query (str): The search query.
+    Returns:
+        list: A list of dictionaries containing the URL, text content, and table data for each scraped page.
+    """
+    num_results = 2
+    url = 'https://www.google.com/search'
+    params = {'q': query, 'num': num_results}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.3'}
+    
+    inference_logger.info(f"Performing google search with query: {query}\nplease wait...")
+    response = requests.get(url, params=params, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    urls = [result.find('a')['href'] for result in soup.find_all('div', class_='tF2Cxc')]
+    
+    inference_logger.info(f"Scraping text from urls, please wait...") 
+    [inference_logger.info(url) for url in urls]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(lambda url: (url, requests.get(url, headers=headers).text if isinstance(url, str) else None), url) for url in urls[:num_results] if isinstance(url, str)]
+        results = []
+        for future in concurrent.futures.as_completed(futures):
+            url, html = future.result()
+            soup = BeautifulSoup(html, 'html.parser')
+            paragraphs = [p.text.strip() for p in soup.find_all('p') if p.text.strip()]
+            text_content = ' '.join(paragraphs)
+            text_content = re.sub(r'\s+', ' ', text_content)
+            table_data = [[cell.get_text(strip=True) for cell in row.find_all('td')] for table in soup.find_all('table') for row in table.find_all('tr')]
+            if text_content or table_data:
+                results.append({'url': url, 'content': text_content, 'tables': table_data})
+    return results
 
 @tool
 def get_current_stock_price(symbol: str) -> float:
@@ -199,28 +289,11 @@ def get_company_profile(symbol: str) -> dict:
     except Exception as e:
         print(f"Error fetching company profile for {symbol}: {e}")
         return {}
-    
-@tool
-def execute_code(code_string):
-    """
-    Execute the provided Python code string using exec.
-
-    Parameters:
-    - code_string (str): The Python code to be executed.
-
-    Returns:
-    None
-
-    Note:
-    Use this function with caution, as executing arbitrary code can pose security risks.
-    """
-    try:
-        exec(code_string)
-    except Exception as e:
-        print(f"An error occurred: {e}")
 
 def get_openai_tools() -> List[dict]:
     functions = [
+        code_interpreter,
+        google_search_and_scrape,
         get_current_stock_price,
         get_company_news,
         get_company_profile,
@@ -229,9 +302,7 @@ def get_openai_tools() -> List[dict]:
         get_key_financial_ratios,
         get_analyst_recommendations,
         get_dividend_data,
-        #get_historical_price_data,
-        get_technical_indicators,
-        execute_code
+        get_technical_indicators
     ]
 
     tools = [convert_to_openai_tool(f) for f in functions]

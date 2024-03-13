c60a2c22df3fd248f402d9edd877584add6fa9d5
interstellarninja <ninjainterstellar@gmail.com>
2024-03-13 14:06:56 +0000

    returning code execution errors

--- functions.py
+++ functions.py
@@ -12,7 +12,7 @@ from langchain.tools import tool
 from langchain_core.utils.function_calling import convert_to_openai_tool
 
 @tool
-def code_interpreter(code_markdown: str) -> dict:
+def code_interpreter(code_markdown: str) -> dict | str:
     """
     Execute the provided Python code string on the terminal using exec.
     The string should contain valid, executable and pure python code in markdown syntax.
@@ -55,7 +55,9 @@ def code_interpreter(code_markdown: str) -> dict:
         return result_dict
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        error_message = f"An error occurred: {e}"
+        inference_logger.error(error_message)
+        return error_message
 
 @tool
 def google_search_and_scrape(query: str) -> dict:
